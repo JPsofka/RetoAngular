d4bdf93e9a11af43f985fabddfa01c56c394612a
Jesús Portillo Villa <jesus.portillo@sofka.com.co>
2023-01-04 20:38:13 -0500

    product funtions are completed

--- src/app/components/product-list/product-list.component.ts
+++ src/app/components/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ProductService } from 'src/app/services/product.service';
+import { Product } from 'src/Product';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
+  products: any;
+  currentProduct!: null;
+  currentIndex = -1;
+  product = {
+    productId:0,
+    name:"",
+    inInventory:0,
+    enabled:0,
+    min:0,
+    max:0
+  };
+  productEmpty = {
+    productId:0,
+    name:"",
+    inInventory:0,
+    enabled:0,
+    min:0,
+    max:0
+  };
+
+  constructor(private productService: ProductService) { }
 
   ngOnInit(): void {
+    this.getAllProducts()
   }
 
+  getAllProducts():void{
+    this.productService.getAll()
+      .subscribe({next: (values)=> {
+        this.products = values
+        console.log(this.products)
+      }})
+  }
+
+  refresh(): void {
+    this.getAllProducts();
+    this.currentProduct = null;
+    this.currentIndex = -1;
+    this.setCurrentProduct(this.productEmpty);
+  }
+
+  setCurrentProduct(productSelected:any): void {
+    this.currentProduct = productSelected;
+    this.product = productSelected;
+  }
+
+  delete(id:number):void {
+    this.productService.delete(id).subscribe({complete: ()=> this.refresh()});
+  }
+
+  edit(id:number, product:any):void {
+    this.productService.update(id, product).subscribe({complete: ()=> {
+      this.refresh;
+      this.setCurrentProduct(this.productEmpty);
+    }})
+  }
 }
